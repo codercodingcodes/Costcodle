1cb2520b7fe49fc72c87337c27d99143266b415e
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-11 15:37:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -398,6 +398,7 @@ def register():
     userID = request.json["userID"]
     interID = getInterID(userID,sessionID)
     updateInterID(userID,interID,sessionID)
+    return Response("registered",status=200)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
